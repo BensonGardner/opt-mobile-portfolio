b97d7a198010afe851e03c71bbb70185a2a0b548
bensongardner <bensongardner@yahoo.com>
2017-11-08 20:57:16 -0600

    reconfig: Correct file structure

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,20 @@ var gulp = require('gulp'),
 gulp.task('minify', function() {
   return gulp.src('source/**/*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest('dist'));
 });
  
 gulp.task('minify-css', function() {
   return gulp.src('source/**/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest(''));
+    .pipe(gulp.dest('dist'));
 });
  
 gulp.task('compress', function (cb) {
     pump([
         gulp.src('source/**/*.js'),
         uglify(),
-        gulp.dest('')
+        gulp.dest('dist')
     ],
     cb
   );
@@ -33,7 +33,7 @@ gulp.task('compress', function (cb) {
 
 // image processing
 gulp.task('images', function() {
-  var final = '';
+  var final = 'dist';
   return gulp.src('source/**/*')
     .pipe(imagemin({ optimizationLevel: 7 }))
     .pipe(gulp.dest(final));
